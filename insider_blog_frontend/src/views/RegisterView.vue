2ef0e2346ec845ca5cd147a9d371722d298fddb3
Owzok <martin.perezbonany@utec.edu.pe>
2022-07-07 16:39:54 -0500

    error handling

--- insider_blog_frontend/src/views/RegisterView.vue
+++ insider_blog_frontend/src/views/RegisterView.vue
@@ -2,52 +2,57 @@
   <header class="site-header">
     <form @submit.prevent="handleSubmit">
       <h3>Sign Up</h3>
-      <div class="form-group">
-        <label>Username</label>
-        <input
-          type="text"
-          name="username"
-          v-model="username"
-          class="form-control"
-        />
-      </div>
+      <div class="content-section">
 
-      <div class="form-group">
-        <label>Email</label>
-        <input type="email" name="email" v-model="email" class="form-control" />
-      </div>
+        <div class="form-group">
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            v-model="username"
+            class="form-control"
+          />
+        </div>
 
-      <div class="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          v-model="password"
-          class="form-control"
-        />
-      </div>
+        <div class="form-group">
+          <label>Email</label>
+          <input type="email" name="email" v-model="email" class="form-control" />
+        </div>
 
-      <div class="form-group">
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          name="confirm_pass"
-          v-model="confirm_pass"
-          class="form-control"
-        />
-      </div>
+        <div class="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            class="form-control"
+          />
+        </div>
 
-      <div class="form-group">
-        <button class="btn btn-primary btn-block" v-on:click="handleSubmit()">
-          Sign Up
-        </button>
+        <div class="form-group">
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirm_pass"
+            v-model="confirm_pass"
+            class="form-control"
+          />
+        </div>
+
+        <div class="form-group">
+          <button class="btn btn-primary btn-block" v-on:click="handleSubmit()">
+            Sign Up
+          </button>
+        </div>
       </div>
     </form>
+    <div class="alert alert-danger" role="alert" v-if="error">
+      {{error_msg}}
+    </div>
   </header>
 </template>
 
 <script>
-import router from "@/router";
 import axios from "axios";
 
 export default {
@@ -59,19 +64,35 @@ export default {
       email: "",
       password: "",
       confirm_pass: "",
+      error: false,
     };
   },
   methods: {
     async handleSubmit() {
-      if (this.password == this.confirm_pass) {
-        await axios.post("/users", {
-          username: this.username,
-          email: this.email,
-          password: this.password,
-        });
-        await router.push("/login");
+      this.error = false
+      await axios.post(
+        "/users",
+        {
+            username: this.username,
+            email: this.email,
+            password: this.password,
+        }
+      ).catch(
+        err => {
+          console.log(err)
+          this.error = true;
+          this.error_msg = "Invalid Form! Please try again."
+        }
+      );
+      if(error == false){
+        this.$router.push("/login")
       }
     },
   },
-};
+}
 </script>
+
+<style>
+  @import '../assets/styles.css'
+
+</style>
